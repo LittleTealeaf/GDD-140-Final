eeeea9b3f4e143e3e5d9a60288193022270cd8b1
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-05-05 18:37:09 +0000

    deploy: 0c69d4df1ae568f939deb1a9bd100a0504921f20

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
+/*
+General Idea: Have a generative art that is seeded by the current minute
+*/
+
 function setup() {
-    createCanvas(600,600);
+    createCanvas(windowWidth - 20, windowHeight - 20);
 }
 
 function draw() {
